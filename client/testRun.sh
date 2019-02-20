ed625571b7f4c394ddd014e4c7e1f8af657102ea
Johan van Soest <johan.vansoest@maastro.nl>
2019-02-20 21:48:14 +0100

    testRun compliant with compose

--- client/testRun.sh
+++ client/testRun.sh
@@ -1,10 +1,4 @@
 docker rmi jvsoest/ptm_client
 docker build -t jvsoest/ptm_client ./
 
-docker volume create ioData
-
-docker run --rm \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    -v $(pwd)/config.json:/ptm_client/config.json \
-    -v ioData:/ioData \
-    jvsoest/ptm_client
+docker-compose up
